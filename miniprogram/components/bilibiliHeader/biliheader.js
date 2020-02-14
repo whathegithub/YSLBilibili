3fd02166910a322f1161d8dd8365d1890e7cbba0
jiangbowen_ <18853716897@163.com>
2020-02-14 15:12:53 +0800

    scrollview

---
+++ miniprogram/components/bilibiliHeader/biliheader.js
@@ -0,0 +1,30 @@
+// components/bilibiliHeader/biliheader.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    headText:{
+      type:String,
+      value:"下载App"
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  downLoadApp(){
+    console.log("下载app....");
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
